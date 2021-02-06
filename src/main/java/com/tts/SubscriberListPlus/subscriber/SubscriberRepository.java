1a4c0158630ac6840d325229465f46432d484e42
LLJ3288 <74459081+LLJ3288@users.noreply.github.com>
2021-02-06 14:37:44 -0600

    First commit

---
+++ src/main/java/com/tts/SubscriberListPlus/subscriber/SubscriberRepository.java
@@ -0,0 +1,7 @@
+package com.tts.SubscriberListPlus.subscriber;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface SubscriberRepository extends CrudRepository <Subscriber, Long> {
+
+}
